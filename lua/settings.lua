e009ce32e3a62c3c6665bf8d5de63efd2e748dc8
Tai-Github <hoangductai2007@gmail.com>
2021-06-26 08:33:13 +0700

    lua/settings.lua: Fix

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,4 +1,3 @@
--- Basic settings
 G.mapleader = ' '                          -- Set key <leader>
 O.mouse = 'a'                              -- Enable your mouse
 O.fileencoding = "utf-8"                   -- The encoding written to file
@@ -13,13 +12,9 @@ O.swapfile = false                         -- Disable swap files
 O.backup = false                           -- Disable backup
 O.writebackup = false                      -- Disable write backup
 CMD('autocmd BufWritePre * %s/\\s\\+$//e') -- Remove trailing whitespace on save
-
--- Syntax highlight settings
 CMD('syntax on')                           -- Enable syntax highlight
 WO.cursorline = true                       -- Enable highlighting of the current line
 O.termguicolors = true                     -- Set term gui colors most terminals support this
-
--- Tab settings
 CMD('set expandtab')                       -- Converts tabs to spaces
 CMD('set tabstop=2')                       -- Insert 2 spaces for a tab
 CMD('set shiftwidth=2')                    -- Change the number of space characters inserted for indentation
