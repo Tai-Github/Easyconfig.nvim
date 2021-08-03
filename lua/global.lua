5987a3dabe85ebd595102c7c9b4aff8efd4094cf
TaiK7 <hoangductai2007@gmail.com>
2021-08-03 19:17:30 +0700

    Fix floaterm, nvimtree config

--- lua/global.lua
+++ lua/global.lua
@@ -1,12 +1,12 @@
-O = vim.o
-BO = vim.bo
-WO = vim.wo
-CMD = vim.cmd
-G = vim.g
-FN = vim.fn
-KEYMAP = vim.api.nvim_set_keymap
-SHCMD = vim.fn.system
+O           = vim.o
+BO          = vim.bo
+WO          = vim.wo
+CMD         = vim.cmd
+G           = vim.g
+FN          = vim.fn
+KEYMAP      = vim.api.nvim_set_keymap
+SHCMD       = vim.fn.system
 CONFIG_PATH = vim.fn.stdpath('config')
-DATA_PATH = vim.fn.stdpath('data')
-OPTION1 = {noremap = true, silent = true}
-OPTION2 = {noremap = true, silent = true, expr = true}
+DATA_PATH   = vim.fn.stdpath('data')
+OPTION1     = {noremap = true, silent = true}
+OPTION2     = {noremap = true, silent = true, expr = true}
--- lua/nv-floaterm/init.lua
+++ lua/nv-floaterm/init.lua
@@ -1,10 +1,18 @@
 -- Config
 G.floaterm_autoclose = 2
+G.floaterm_width     = 0.8
+G.floaterm_height    = 0.9
+
+-- Highlight
+CMD('hi FloatermBorder guibg=normal')
 
 -- Key bindings
-G.floaterm_keymap_kill = '<Esc><Esc>'
-KEYMAP('n', '<Leader>t;',
-  ':FloatermNew --height=0.9 --width=0.8<CR>', OPTION1)
+G.floaterm_keymap_kill   = '<Esc><Esc>'
+G.floaterm_keymap_new    = '<M-t>t'
+G.floaterm_keymap_prev   = '<M-t>h'
+G.floaterm_keymap_next   = '<M-t>n'
+G.floaterm_keymap_toggle = '<M-t>h'
 
-KEYMAP('n', '<Leader>tl',
-  ':FloatermNew --height=0.9 --width=0.8 lazygit<CR>', OPTION1)
+-- Git
+KEYMAP('n', '<M-t>g',
+  ':FloatermNew lazygit<CR>', OPTION1)
