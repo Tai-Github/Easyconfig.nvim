f95ad7bc06d487f36bceecc56b6c1ef9404d387c
TaiHoang <hoangductai2007@gmail.com>
2023-05-22 16:48:44 +0700

    fix clipboard for wsl

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,7 @@
+-- Check is installed
+local _, theme = pcall(require, 'tokyodark')
+if not _ then return end
+
 G.tokyodark_transparent_background = false
 G.tokyodark_enable_italic_comment = false
 G.tokyodark_enable_italic = false
